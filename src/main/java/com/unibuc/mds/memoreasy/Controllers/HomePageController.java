9e82a9b5cfefbd0c163936acd6cd278a5b610fea
Telu Mihai <telu.mihai.7.com@gmail.com>
2025-05-01 19:55:11 +0300

    Conceptual diagram and connection with database

--- src/main/java/com/unibuc/mds/memoreasy/Controllers/HomePageController.java
+++ src/main/java/com/unibuc/mds/memoreasy/Controllers/HomePageController.java
@@ -16,26 +16,5 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomePageController {
-//    @FXML
-//    private Button homeButton;
-//
-//    @FXML
-//    private Button FlashcardButton;
-//
-//    public void goToCategories(ActionEvent event) throws Exception{
-//        Parent root= FXMLLoader.load(getClass().getResource("/com/unibuc/mds/memoreasy/Views/AllCategories/AllCategoriesView.fxml"));
-//        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        Scene scene=new Scene(root);
-//        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-//        stage.setScene(scene);
-//        stage.show();
-//
-//    }
-//
-//
-//    @Override
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        FontIcon icon = new FontIcon("fas-home");
-//        homeButton.setGraphic(icon);
-//    }
+
 }
